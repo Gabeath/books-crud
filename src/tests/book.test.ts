ebeaa56b529a2d63aee92a0f7369d1806a7f0f6e
Gabriel Zambuzi <gabriel.zambuzi@mblabs.com.br>
2022-03-23 21:30:51 -0300

    feat: update book tests

--- src/tests/book.test.ts
+++ src/tests/book.test.ts
@@ -86,7 +86,7 @@ describe('Book creation tests', () => {
 });
 
 describe('Books list paginated', () => {
-  it('should return a books list paginated', async () => {
+  it('should list the books paginated', async () => {
     const response = await request(server)
       .get('/api/book?skip=0&take=10');
 
@@ -117,7 +117,7 @@ describe('Books list paginated', () => {
 });
 
 describe('Book details', () => {
-  it('should return the book details', async () => {
+  it('should get the book details', async () => {
     const bookToCreate = {
       name: faker.lorem.words(3),
       author: faker.name.findName(),
@@ -180,5 +180,135 @@ describe('Book details', () => {
       .get('/api/book/0a133a67-78d0-410e-a3b8-ed33cc88066b');
 
     expect(response.status).toBe(400);
+  });
+});
+
+describe('Book update', () => {
+  it('should update the book', async () => {
+    const bookToCreate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+      sbn: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
+    } as Book;
+
+    const bookToUpdate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+    } as Book;
+
+    const bookCreated = await prisma.book.create({
+      data: bookToCreate,
+    });
+
+    const response = await request(server)
+      .put(`/api/book/${bookCreated.id}`)
+      .send(bookToUpdate);
+
+    expect(response.status).toBe(200);
+
+    await prisma.book.delete({
+      where: { id: bookCreated.id },
+    });
+  });
+
+  it('should return the book updated', async () => {
+    const bookToCreate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+      sbn: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
+    } as Book;
+
+    const bookToUpdate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+    } as Book;
+
+    const bookCreated = await prisma.book.create({
+      data: bookToCreate,
+    });
+
+    const response = await request(server)
+      .put(`/api/book/${bookCreated.id}`)
+      .send(bookToUpdate);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty('author');
+    expect(response.body).toHaveProperty('description');
+    expect(response.body).toHaveProperty('inventory');
+    expect(response.body).toHaveProperty('sbn');
+
+    await prisma.book.delete({
+      where: { id: bookCreated.id },
+    });
+  });
+
+  it('should not update the book with invalid id', async () => {
+    const bookToUpdate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+    } as Book;
+
+    const response = await request(server)
+      .put('/api/book/123')
+      .send(bookToUpdate);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not update the book with not found id', async () => {
+    const bookToUpdate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+    } as Book;
+
+    const response = await request(server)
+      .put('/api/book/0a133a67-78d0-410e-a3b8-ed33cc88066b')
+      .send(bookToUpdate);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not update the book with inventory less than zero', async () => {
+    const bookToCreate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: faker.datatype.number(100),
+      sbn: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
+    } as Book;
+
+    const bookToUpdate = {
+      name: faker.lorem.words(3),
+      author: faker.name.findName(),
+      description: faker.lorem.sentence(),
+      inventory: -1,
+    } as Book;
+
+    const bookCreated = await prisma.book.create({
+      data: bookToCreate,
+    });
+
+    const response = await request(server)
+      .put(`/api/book/${bookCreated.id}`)
+      .send(bookToUpdate);
+
+    expect(response.status).toBe(400);
+
+    await prisma.book.delete({
+      where: { id: bookCreated.id },
+    });
   });
 });
